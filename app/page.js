eb845a2d6e2718546c9c1ed2b9419c0b4d8814a4
Macro404 <marcus.westlund1994@gmail.com>
2022-11-21 16:30:16 +0100

    Added fetch for products

--- app/page.js
+++ app/page.js
@@ -2,27 +2,15 @@ import Phone from "./(components)/Phone";
 import Subscription from "./(components)/Subscription";
 
 
-export default function Home(){
+export default async function Home(){
+
+    const products = await fetchProducts();
 
     return <h1>Here goes content</h1>
 }
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
-/*
-export async function getStaticProps() {
-    // Call an external API endpoint to get posts.
-    // You can use any data fetching library
+async function fetchProducts(){
     const res = await fetch('https://doubleshot-app.azurewebsites.net/api',
     {mode:cors});
-    const products = await res.json()
-  
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-      props: {
-        products,
-      },
-    }
-  }*/
+    return res.json;
+}
