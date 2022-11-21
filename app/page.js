1860ec5ca941b64929342d4d7f576619c3db2727
Macro404 <marcus.westlund1994@gmail.com>
2022-11-21 16:17:36 +0100

    trying without getStaticProps

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Phone from "./(components)/Phone";
 import Subscription from "./(components)/Subscription";
 
 
-export default function Home({products}){
+export default function Home(){
 
     return <h1>Here goes content</h1>
 }
@@ -10,6 +10,7 @@ export default function Home({products}){
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries.
+/*
 export async function getStaticProps() {
     // Call an external API endpoint to get posts.
     // You can use any data fetching library
@@ -24,4 +25,4 @@ export async function getStaticProps() {
         products,
       },
     }
-  }
+  }*/
