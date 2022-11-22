f2852cb1c7a25fe5809b27d77afb80af71dd1ac0
Macro404 <marcus.westlund1994@gmail.com>
2022-11-22 09:38:04 +0100

    testing cors

--- app/page.js
+++ app/page.js
@@ -5,12 +5,13 @@ import Subscription from "./(components)/Subscription";
 export default async function Home(){
 
     const products = await fetchProducts();
+    console.log(products);
 
     return <h1>Here goes content</h1>
 }
 
 async function fetchProducts(){
-    const res = await fetch('https://doubleshot-app.azurewebsites.net/api',
+    const res = await fetch('http   ://doubleshot-app.azurewebsites.net/api', 
     {mode:cors});
     return res.json;
 }
