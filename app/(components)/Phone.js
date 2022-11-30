c67ee9cdff5a08c61eeaa38556d1ca8d3a05480d
Macro404 <marcus.westlund1994@gmail.com>
2022-11-30 13:37:08 +0100

    Added scrollable images for phones page

--- app/(components)/Phone.js
+++ app/(components)/Phone.js
@@ -5,24 +5,32 @@ import CartBtn from './CartBtn';
 
 export default function Phone({ phone }){
     const imageList = phone.images.split(',');
-    return <div className='phone-container'>
-        <div className='image-container' >
-        <Image
-      src={imageList[0]}
-      alt="a phone"
-      width={180}
-      height={300}
-      className="image"
-    />
+    const images = imageList.map(image => {
+        return (
+        <Image 
+            key={image}
+            src={image}
+            alt="image"
+            width={180}
+            height={300}
+        />
+    )})
+    return <div className='phone-card'>
+        <details><summary>
+        <div className='phone-image-container' >
+        {images[0]}
     </div>
+    </summary>
+    <div className='phone-image-dropdown'>{images.slice(1)}</div>
+    </details>
     <div className='phone-details'>
         <h3>{phone.model}</h3>
-        <p>{phone.camera}</p>
-        <p>{phone.cpu}</p>
-        <p>{phone.battery}</p>
-        <p>{phone.screen}</p>
+        <p>Display: {phone.screen}</p>
+        <p>Camera: {phone.camera}</p>
+        <p>CPU: {phone.cpu}</p>
+        <p>Battery: {phone.battery}</p>
         <p>{phone.price}:-</p>
-        <CartBtn item={phone}></CartBtn>
+        <div className='phone-page-cart-button'><CartBtn item={phone}></CartBtn></div>
         </div>
     </div>
 }
