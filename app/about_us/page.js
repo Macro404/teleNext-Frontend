a65a4c20ed40967895c2b0df450ad1a054b75ee1
mvedesti <larsmtv@gmail.com>
2022-12-01 12:30:47 +0100

    edited the about us page

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -5,14 +5,14 @@ export default function AboutUs(){
         <article>
             <p>This is the final project for mob Doubleshot consisting of Marcus Westlund (Stockholm) and Michael Vedestig (Oslo) at &lt;/salt&gt;'s Fullstack Java bootcamp.</p>
             <h2>Site features</h2>
-            <p>We've built a telecom site featuring products in the form of Phones and subscriptions. We use google validation to enable signups to the page as well as for validating access where necessary.</p>
+            <p>We've built a telecom site featuring products in the form of phones and subscriptions. We use google validation to enable signups to the page as well as for validating access.</p>
             <h2>Tech stack</h2>
             <h3>Front-end</h3>
-            <p>The front-end was built using next-js.</p>
+            <p>The front-end was built using Next.js.</p>
             <h3>Back-end</h3>
             <p>The back-end is a REST api built using Spring.</p>
             <h3>Data storage</h3>
-            <p>All of our data is stored in a postgres database.</p>
+            <p>All of our data is stored in a postgreSQL database.</p>
             <h3>Cloud</h3>
             <p>Front-end, back-end and our database are all hosted by Azure.</p>
         </article>
